b0771a455e16709c2943b2d2a2cd9a9b8e630930
hailneni <ailneniharishrao7@gmail.com>
2023-06-05 19:36:03 -0400

    student

---
+++ Person.java
@@ -0,0 +1,14 @@
+package first;
+
+public class Person {
+
+	public static void main(String[] args) {
+
+		int pid=1;
+		String address="345 Luxury,NC";
+		int age=12;
+		String phone="+14764664646";
+		System.out.println(pid+address+age+phone);
+	}
+
+}
