ab6f501724abc0990f7c06ac1e41d6ed9da36092
hailneni <ailneniharishrao7@gmail.com>
2023-06-05 17:52:49 -0400

    move file to repo1

---
+++ Student.java
@@ -0,0 +1,13 @@
+package first;
+
+public class Student {
+
+	public static void main(String[] args) {
+int stuid=1;
+String name="harish";
+int age=22;
+int grade=10;
+System.out.println(stuid+name+ age+grade );
+	}
+
+}
